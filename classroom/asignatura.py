6dee2ce00176af5693f5a0fec9f9b11f8fa3fead
secadavida <142696326+secadavida@users.noreply.github.com>
2023-09-03 16:52:23 +0000

    taller 4 python

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,7 @@
 class Asignatura:
+	def __init__(self, nombre, salon="remoto"):
+		self._nombre = nombre
+		self._salon = salon
 
-    def __init__(self, nombre, salon):
-        self._nombre = nombre
-        self._salon = salon
-
-    # def __str__(self):
-    #     pass
+	def __str__(self):
+		return self._nombre + " " + self._salon
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,31 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+	grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
-        self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+	def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
+		self._grupo = grupo
+		self._asignaturas = asignaturas
+		self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+	def listadoAsignaturas(self, **kwargs):
+		for x in kwargs.values():
+			self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+	def agregarAlumno(self, alumno, lista=None):
+		if not lista:
+			lista = []
 
-    # def __str__(self):
-    #     pass
+		lista.append(alumno)
+		self.listadoAlumnos = self.listadoAlumnos + lista
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+	def __str__(self):
+		return "Grupo de estudiantes: " + self._grupo
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
+	@ classmethod
+	def asignarNombre(cls, nombre="Grado 1"):
+		cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+	@ classmethod
+	def asignarNombre(cls, nombre="Grado 6"):
+		cls.grado = nombre
